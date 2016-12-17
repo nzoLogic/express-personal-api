e4ece50f523af88a7cb4c01f57e9041ac2784da3
nzoLogic <nzoLogic@gmail.com>
2016-12-16 16:49:32 -0800

    logging profile api and deployed minor ajax requests

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,21 @@
 console.log("Sanity Check: JS is working!");
 
-$(document).ready(function(){
+$(document).ready(function() {
+    var profileLocation = $('#profile-location').html(),
+        profileHB = Handlebars.compile(profileLocation);
+        console.log(profileLocation);
+    $.ajax({
+        method: 'GET',
+        url: '/api/profile',
+        success: handleSuccess,
+        error: handleError,
+        dataType: 'json'
+    });
 
-// $.ajax({
-//   method: 'GET',
-//   url: '../views/server.js'
-// })
+    function handleSuccess(data){
+      console.log(data);
+    }
+    function handleError(xhr, err){
+      console.log('error');
+    }
 });
